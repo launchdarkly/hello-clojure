16b27ce6a8482443d56e082a3bf1f2b35c24af20
Alex Engelberg <aengelberg@launchdarkly.com>
2022-05-10 12:38:48 -0700

    Add CircleCI

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hello-clojure "0.0.1"
   ;; Don't warn about version ranges
   :pedantic? false
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
 
                  [com.launchdarkly/launchdarkly-java-server-sdk "[5.0,6.0)"]
                  [org.slf4j/slf4j-simple "1.7.22"]]
