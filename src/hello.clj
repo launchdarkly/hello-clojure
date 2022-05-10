fb6b794b2c1b8e812ba07d0c040bd81ac93093d7
Alex Engelberg <aengelberg@launchdarkly.com>
2022-05-09 18:18:30 -0700

    Add Clojure project, hello source

---
+++ src/hello.clj
@@ -0,0 +1,27 @@
+(ns hello
+  (:import
+   (com.launchdarkly.sdk.server LDClient LDConfig)
+   (com.launchdarkly.sdk LDUser LDUser$Builder)))
+
+;; Put your SDK key here
+(def sdk-key "")
+
+;; Set the feature flag you want to evaluate
+(def flag-key "my-boolean-flag")
+
+(defn -main
+  [& args]
+  (when (= "" sdk-key)
+    (println "Please edit hello.clj to set sdk-key to your LaunchDarkly SDK key first.")
+    (System/exit 1))
+  ;; Here we set up the client, and use `with-open` to ensure the client gets cleanly shutdown before
+  ;; the program exits, to deliver any analytics events still waiting in the buffer.
+  (with-open [client (LDClient. sdk-key)]
+    (let [;; Set up the user properties. This user should appear on your LaunchDarkly
+          ;; users dashboard soon after you run the demo.
+          user (.. (LDUser$Builder. "example-user-key")
+                   (name "Sandy")
+                   (build))
+          flag-value (.boolVariation client flag-key user false)]
+      (printf "Feature flag '%s' is %s for this user\n"
+              flag-key flag-value))))
